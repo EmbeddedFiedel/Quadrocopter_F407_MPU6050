52280c7e727af6a47fe6b8155e4210df3e720bd0
Karsten <karsten-bade@web.de>
2012-12-21 16:14:40 +0100

    Update Quad/Regelung.cpp
    Regelung.cpp aufgeräumt
    Prio Syncthread > Prio Printthread

--- Quad/Regelung.cpp
+++ Quad/Regelung.cpp
@@ -77,48 +77,40 @@ volatile unsigned short tmp111;
 bool_t datalog_regelung_opened = 0;
 bool_t datalog_regelung_syncing = 0;
 static TimeMeasurement regelungdatalogsync_tmup;
-	/*
- * Working area for LageSync
+/*
+ * Working area for RegelungSyncThread
  */
 static WORKING_AREA(RegelungSyncThreadWorkingArea, 2048);
 /*
- * LageSync
+ * RegelungSyncThread
  */
-static msg_t RegelungSyncthread(void *arg) {
- 
-  systime_t time = chTimeNow();     // Tnow
-  while (TRUE) 
+static msg_t RegelungSyncthread(void *arg) 
+{
+  	while (TRUE) 
 	{
 		if(Datalogger_ready() && !datalog_regelung_opened)
 		{
-			//time +=  MS2ST(500);           // Next deadline
-			//rc = f_mkfs(0,0,0);
 				rc_datalog = f_open(&Fil_regelung, ("QuadRege.TXT"), FA_WRITE | FA_CREATE_ALWAYS);
 				if(rc_datalog != FR_OK)
 				{
 					chprintf((BaseChannel *) &SD2, "SD Quadregelung.TXT: f_open() failed %d\r\n", rc_datalog);
 					chThdSleepMilliseconds(10);
-				}	
-/*				
-				rc_datalog = f_sync(&Fil_regelung);
-				if(rc_datalog != FR_OK)
-				{
-					chprintf((BaseChannel *) &SD2, "SD Quadregelung.TXT: f_sync() failed %d\r\n", rc_datalog);
-				}	
-				*/
-				f_printf(&Fil_regelung, "Time_regelung;inSchub;inNickSollLage;inRollSollLage;inYawSollLage;inNickIstLage;inRollIstLage;inYawIstLage;inNickIstV;inRollIstV;inYawIstV;ea_Nick;ea_Roll;ea_Yaw;ia_Nick;ia_Roll;ia_Yaw;Soll_v_Nick;Soll_v_Roll;Soll_v_Yaw;ii_Nick;ii_Roll;ii_Yaw;pi_Nick;pi_Roll;pi_Yaw;di_Nick;di_Roll;di_Yaw;v_Nick_tp1;v_Roll_tp1;v_Yaw_tp1;aNick;aRoll;aYaw;outMotor1;outMotor2;outMotor3;outMotor4\r\n");
-				//f_printf(&Fil_regelung, "Time_reg");
-				rc_datalog = f_sync(&Fil_regelung);	
-				if(rc_datalog != FR_OK)
-				{
-					chprintf((BaseChannel *) &SD2, "SD Quadregelung.TXT: initial f_sync() failed %d\r\n", rc_datalog);
-					chThdSleepMilliseconds(10);
-				}	
+				}
 				else
 				{
-					chprintf((BaseChannel *) &SD2, "SD Quadregelung.TXT: opened successfull\r\n");
-					chThdSleepMilliseconds(10);
-					datalog_regelung_opened = TRUE;	
+					f_printf(&Fil_regelung, "Time_regelung;inSchub;inNickSollLage;inRollSollLage;inYawSollLage;inNickIstLage;inRollIstLage;inYawIstLage;inNickIstV;inRollIstV;inYawIstV;ea_Nick;ea_Roll;ea_Yaw;ia_Nick;ia_Roll;ia_Yaw;Soll_v_Nick;Soll_v_Roll;Soll_v_Yaw;ii_Nick;ii_Roll;ii_Yaw;pi_Nick;pi_Roll;pi_Yaw;di_Nick;di_Roll;di_Yaw;v_Nick_tp1;v_Roll_tp1;v_Yaw_tp1;aNick;aRoll;aYaw;outMotor1;outMotor2;outMotor3;outMotor4\r\n");	
+					rc_datalog = f_sync(&Fil_regelung);	
+					if(rc_datalog != FR_OK)
+					{
+						chprintf((BaseChannel *) &SD2, "SD Quadregelung.TXT: initial f_sync() failed %d\r\n", rc_datalog);
+						chThdSleepMilliseconds(10);
+					}	
+					else
+					{
+						chprintf((BaseChannel *) &SD2, "SD Quadregelung.TXT: opened successfull\r\n");
+						chThdSleepMilliseconds(10);
+						datalog_regelung_opened = TRUE;	
+					}
 				}
 				chThdSleepMilliseconds(10);
 		}
@@ -126,14 +118,14 @@ static msg_t RegelungSyncthread(void *arg) {
 		{
 			datalog_regelung_syncing = 1;
 			rc_datalog = f_sync(&Fil_regelung);
-				if(rc_datalog != FR_OK)
-				{
-					chprintf((BaseChannel *) &SD2, "SD Quadregelung.TXT: datalog f_sync() failed %d\r\n", rc_datalog);
-				}	
-				else
-				{
-					chprintf((BaseChannel *) &SD2, "SD Quadregelung.TXT: datalog f_sync() succeeded\r\n");
-				}
+			if(rc_datalog != FR_OK)
+			{
+				chprintf((BaseChannel *) &SD2, "SD Quadregelung.TXT: datalog f_sync() failed %d\r\n", rc_datalog);
+			}	
+			else
+			{
+				chprintf((BaseChannel *) &SD2, "SD Quadregelung.TXT: datalog f_sync() succeeded\r\n");
+			}
 			datalog_regelung_syncing = 0;
 			chThdSleepMilliseconds(1000);
 		}
@@ -141,7 +133,7 @@ static msg_t RegelungSyncthread(void *arg) {
 		{
 			chThdSleepMilliseconds(10);
 		}		
-  }
+  	}
 }
 
 	/*
@@ -159,7 +151,7 @@ static msg_t RegelungPrintthread(void *arg)
 		{
 			systime_t time = chTimeNow();     // Tnow
 			chprintf((BaseChannel *) &SD2, "Printing:%d\r\n",time);
-				f_printf(&Fil_regelung, "%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d\r\n",
+			f_printf(&Fil_regelung, "%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d\r\n",
 							regelung_timebuffer[readcounter],
 							regelung_databuffer[readcounter][0],
 							regelung_databuffer[readcounter][1],
@@ -201,7 +193,6 @@ static msg_t RegelungPrintthread(void *arg)
 							regelung_databuffer[readcounter][38]);
 							readcounter++;
 							if(readcounter >=50)readcounter=0;
-							
 		}
 		chThdSleepMilliseconds(2);
 	}
@@ -210,54 +201,54 @@ static msg_t RegelungPrintthread(void *arg)
 void datalog_regelung(void)
 {
 	uint32_t system_time = chTimeNow();
-			//f_printf(&Fil_regelung, "%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d\r\n",
+	regelung_timebuffer[writecounter] = system_time;
+	regelung_databuffer[writecounter][0]=(int)(inSchub*100);
+	regelung_databuffer[writecounter][1]=(int)(inNickSollLage*100);
+	regelung_databuffer[writecounter][2]=(int)(inRollSollLage*100);
+	regelung_databuffer[writecounter][3]=(int)(inYawSollLage*100);
+	regelung_databuffer[writecounter][4]=(int)(inNickIstLage*100);
+	regelung_databuffer[writecounter][5]=(int)(inRollIstLage*100);
+	regelung_databuffer[writecounter][6]=(int)(inYawIstLage*100);
+	regelung_databuffer[writecounter][7]=(int)(inNickIstV*100);
+	regelung_databuffer[writecounter][8]=(int)(inRollIstV*100);
+	regelung_databuffer[writecounter][9]=(int)(inYawIstV*100);
+	regelung_databuffer[writecounter][11]=(int)(ea_Nick*100);
+	regelung_databuffer[writecounter][12]=(int)(ea_Roll*100);
+	regelung_databuffer[writecounter][13]=(int)(ea_Yaw*100);
+	regelung_databuffer[writecounter][14]=(int)(ia_Nick*100);
+	regelung_databuffer[writecounter][15]=(int)(ia_Roll*100);
+	regelung_databuffer[writecounter][16]=(int)(ia_Yaw*100);
+	regelung_databuffer[writecounter][17]=(int)(Soll_v_Nick*100);
+	regelung_databuffer[writecounter][18]=(int)(Soll_v_Roll*100);
+	regelung_databuffer[writecounter][19]=(int)(Soll_v_Yaw*100);
+	regelung_databuffer[writecounter][20]=(int)(ii_Nick*100);
+	regelung_databuffer[writecounter][21]=(int)(ii_Roll*100);
+	regelung_databuffer[writecounter][22]=(int)(ii_Yaw*100);
+	regelung_databuffer[writecounter][23]=(int)(pi_Nick*100);
+	regelung_databuffer[writecounter][24]=(int)(pi_Roll*100);
+	regelung_databuffer[writecounter][25]=(int)(pi_Yaw*100);
+	regelung_databuffer[writecounter][26]=(int)(di_Nick*100);
+	regelung_databuffer[writecounter][27]=(int)(di_Roll*100);
+	regelung_databuffer[writecounter][28]=(int)(di_Yaw*100);
+	regelung_databuffer[writecounter][29]=(int)(v_Nick_tp1*100);
+	regelung_databuffer[writecounter][30]=(int)(v_Roll_tp1*100);
+	regelung_databuffer[writecounter][31]=(int)(v_Yaw_tp1*100);
+	regelung_databuffer[writecounter][32]=(int)(aNick*100);
+	regelung_databuffer[writecounter][33]=(int)(aRoll*100);
+	regelung_databuffer[writecounter][34]=(int)(aYaw*100);
+	regelung_databuffer[writecounter][35]=(int)(outMotor1);
+	regelung_databuffer[writecounter][36]=(int)(outMotor2);
+	regelung_databuffer[writecounter][37]=(int)(outMotor3);
+	regelung_databuffer[writecounter][38]=(int)(outMotor4);
 	
-						regelung_timebuffer[writecounter] = system_time;
-						regelung_databuffer[writecounter][0]=(int)(inSchub*100);
-						regelung_databuffer[writecounter][1]=(int)(inNickSollLage*100);
-						regelung_databuffer[writecounter][2]=(int)(inRollSollLage*100);
-						regelung_databuffer[writecounter][3]=(int)(inYawSollLage*100);
-						regelung_databuffer[writecounter][4]=(int)(inNickIstLage*100);
-						regelung_databuffer[writecounter][5]=(int)(inRollIstLage*100);
-						regelung_databuffer[writecounter][6]=(int)(inYawIstLage*100);
-						regelung_databuffer[writecounter][7]=(int)(inNickIstV*100);
-						regelung_databuffer[writecounter][8]=(int)(inRollIstV*100);
-						regelung_databuffer[writecounter][9]=(int)(inYawIstV*100);
-						regelung_databuffer[writecounter][11]=(int)(ea_Nick*100);
-						regelung_databuffer[writecounter][12]=(int)(ea_Roll*100);
-						regelung_databuffer[writecounter][13]=(int)(ea_Yaw*100);
-						regelung_databuffer[writecounter][14]=(int)(ia_Nick*100);
-						regelung_databuffer[writecounter][15]=(int)(ia_Roll*100);
-						regelung_databuffer[writecounter][16]=(int)(ia_Yaw*100);
-						regelung_databuffer[writecounter][17]=(int)(Soll_v_Nick*100);
-						regelung_databuffer[writecounter][18]=(int)(Soll_v_Roll*100);
-						regelung_databuffer[writecounter][19]=(int)(Soll_v_Yaw*100);
-						regelung_databuffer[writecounter][20]=(int)(ii_Nick*100);
-						regelung_databuffer[writecounter][21]=(int)(ii_Roll*100);
-						regelung_databuffer[writecounter][22]=(int)(ii_Yaw*100);
-						regelung_databuffer[writecounter][23]=(int)(pi_Nick*100);
-						regelung_databuffer[writecounter][24]=(int)(pi_Roll*100);
-						regelung_databuffer[writecounter][25]=(int)(pi_Yaw*100);
-						regelung_databuffer[writecounter][26]=(int)(di_Nick*100);
-						regelung_databuffer[writecounter][27]=(int)(di_Roll*100);
-						regelung_databuffer[writecounter][28]=(int)(di_Yaw*100);
-						regelung_databuffer[writecounter][29]=(int)(v_Nick_tp1*100);
-						regelung_databuffer[writecounter][30]=(int)(v_Roll_tp1*100);
-						regelung_databuffer[writecounter][31]=(int)(v_Yaw_tp1*100);
-						regelung_databuffer[writecounter][32]=(int)(aNick*100);
-						regelung_databuffer[writecounter][33]=(int)(aRoll*100);
-						regelung_databuffer[writecounter][34]=(int)(aYaw*100);
-						regelung_databuffer[writecounter][35]=(int)(outMotor1);
-						regelung_databuffer[writecounter][36]=(int)(outMotor2);
-						regelung_databuffer[writecounter][37]=(int)(outMotor3);
-						regelung_databuffer[writecounter][38]=(int)(outMotor4);
-						writecounter++;
-						if(writecounter >=50)writecounter=0;
+	writecounter++;
+	
+	if(writecounter >=50)writecounter=0;
 }
 
 void Regelung(void)
 {
-	/// Werte übernehmen
+	/// Werte ï¿½bernehmen
 
 	inSchub = getSchub();
 	
@@ -274,11 +265,11 @@ void Regelung(void)
 	inYawIstV = getRate_yaw();
 
 	/////////////////////////// Nick-Regler berechnen //////////////////////////////////////////
-   //äußerer Regler
-   ea_Nick = (inNickSollLage) - (inNickIstLage+0.08*inNickIstV);  // Eingang in den äußeren Regler
+   //ï¿½uï¿½erer Regler
+   ea_Nick = (inNickSollLage) - (inNickIstLage+0.08*inNickIstV);  // Eingang in den ï¿½uï¿½eren Regler
    if(inSchub > 0.1 && inSchub <=1 && inNickIstLage < 0.2 && inNickIstLage > -0.2)
-   	ia_Nick = 0.01 * ea_Nick + ia_Nick;	//I-Anteil nur nahe der Nulllage verändern  
-   //Soll_v_Nick = ea_Nick*1 + ia_Nick*0.02; //Sollgeschwindigkeit des äußeren Reglers
+   	ia_Nick = 0.01 * ea_Nick + ia_Nick;	//I-Anteil nur nahe der Nulllage verï¿½ndern  
+   //Soll_v_Nick = ea_Nick*1 + ia_Nick*0.02; //Sollgeschwindigkeit des ï¿½uï¿½eren Reglers
    //Soll_v_Nick = inNickSollLage*2.F;
    Soll_v_Nick = 0;
    //innerer Regler
@@ -287,7 +278,7 @@ void Regelung(void)
    pi_Nick = ei_Nick * 0.07;	//p-Anteil
 
    if(inSchub > 0.1 && inSchub <=1 /*&& inNickIstLage < 0.5 && inNickIstLage > -0.5*/)
-   	ii_Nick = 0.08 * ei_Nick + ii_Nick;	//I-Anteil nur nahe der Nulllage verändern 
+   	ii_Nick = 0.08 * ei_Nick + ii_Nick;	//I-Anteil nur nahe der Nulllage verï¿½ndern 
 
    di_Nick = (ei_Nick - ei_Nick_alt)*10; //d-Anteil  
    if (di_Nick > 1.5)
@@ -299,10 +290,10 @@ void Regelung(void)
 
 	/////////////////////////// Roll-Regler berechnen ////////////////////////////////////////// 
 
-   /*ea_Roll = (inRollSollLage) - (inRollIstLage+0.08*inRollIstV);  // Eingang in den äußeren Regler
+   /*ea_Roll = (inRollSollLage) - (inRollIstLage+0.08*inRollIstV);  // Eingang in den ï¿½uï¿½eren Regler
    if(inSchub > 0.1 && inSchub <=1 && inRollIstLage < 0.2 && inRollIstLage > -0.2)
-   	ia_Roll = 0.01 * ea_Roll + ia_Roll;	//I-Anteil nur nahe der Nulllage verändern  
-   Soll_v_Roll = ea_Roll*2 + ia_Roll*0.02; //Sollgeschwindigkeit des äußeren Reglers
+   	ia_Roll = 0.01 * ea_Roll + ia_Roll;	//I-Anteil nur nahe der Nulllage verï¿½ndern  
+   Soll_v_Roll = ea_Roll*2 + ia_Roll*0.02; //Sollgeschwindigkeit des ï¿½uï¿½eren Reglers
    //Soll_v_Roll = inRollSollLage;
 
    //innerer Regler
@@ -310,7 +301,7 @@ void Regelung(void)
    ei_Roll= Soll_v_Roll - v_Roll_tp1;	//Eingang in die innere Regelung
    pi_Roll = ei_Roll * 0.5;	//p-Anteil
    if(inSchub > 0.1 && inSchub <=1 && inRollIstLage < 0.2 && inRollIstLage > -0.2)
-   	ii_Roll = 0.1 * ei_Roll + ii_Roll;	//I-Anteil nur nahe der Nulllage verändern 
+   	ii_Roll = 0.1 * ei_Roll + ii_Roll;	//I-Anteil nur nahe der Nulllage verï¿½ndern 
 
    di_Roll = (ei_Roll - ei_Roll_alt)*20; //d-Anteil  
    if (di_Roll > 1.5)
@@ -320,10 +311,10 @@ void Regelung(void)
 
    aRoll = (pi_Roll + (ii_Roll+37)*0.02 + di_Roll)*567; //Ausgang des inneren Reglers		 */
 
-	ea_Roll = (inRollSollLage) - (inRollIstLage+0.08*inRollIstV);  // Eingang in den äußeren Regler
+	ea_Roll = (inRollSollLage) - (inRollIstLage+0.08*inRollIstV);  // Eingang in den ï¿½uï¿½eren Regler
    if(inSchub > 0.1 && inSchub <=1 && inRollIstLage < 0.2 && inRollIstLage > -0.2)
-   	ia_Roll = 0.01 * ea_Roll + ia_Roll;	//I-Anteil nur nahe der Nulllage verändern  
-   Soll_v_Roll = ea_Roll*2 + ia_Roll*0.02; //Sollgeschwindigkeit des äußeren Reglers
+   	ia_Roll = 0.01 * ea_Roll + ia_Roll;	//I-Anteil nur nahe der Nulllage verï¿½ndern  
+   Soll_v_Roll = ea_Roll*2 + ia_Roll*0.02; //Sollgeschwindigkeit des ï¿½uï¿½eren Reglers
    Soll_v_Roll = inRollSollLage;
 
    //innerer Regler
@@ -331,7 +322,7 @@ void Regelung(void)
    ei_Roll= Soll_v_Roll - v_Roll_tp1;	//Eingang in die innere Regelung
    pi_Roll = ei_Roll * 0.1;	//p-Anteil
    if(inSchub > 0.1 && inSchub <=1 /*&& inRollIstLage < 0.2 && inRollIstLage > -0.2*/)
-   	ii_Roll = 0.01 * ei_Roll + ii_Roll;	//I-Anteil nur nahe der Nulllage verändern 
+   	ii_Roll = 0.01 * ei_Roll + ii_Roll;	//I-Anteil nur nahe der Nulllage verï¿½ndern 
 
 
    di_Roll = (ei_Roll - ei_Roll_alt)*10; //d-Anteil  
@@ -344,10 +335,10 @@ void Regelung(void)
 
 	/////////////////////////// Yaw-Regler berechnen ////////////////////////////////////////// 
 
-   ea_Yaw = (inYawSollLage) - (inYawIstLage+0.08*inYawIstV);  // Eingang in den äußeren Regler
+   ea_Yaw = (inYawSollLage) - (inYawIstLage+0.08*inYawIstV);  // Eingang in den ï¿½uï¿½eren Regler
    if(inSchub > 0.1 && inSchub <=1 && inYawIstLage < 0.2 && inYawIstLage > -0.2)
-   	ia_Yaw = 0.01 * ea_Yaw + ia_Yaw;	//I-Anteil nur nahe der Nulllage verändern  
-   Soll_v_Yaw = ea_Yaw*0.5 + ia_Yaw*0.002; //Sollgeschwindigkeit des äußeren Reglers
+   	ia_Yaw = 0.01 * ea_Yaw + ia_Yaw;	//I-Anteil nur nahe der Nulllage verï¿½ndern  
+   Soll_v_Yaw = ea_Yaw*0.5 + ia_Yaw*0.002; //Sollgeschwindigkeit des ï¿½uï¿½eren Reglers
    //Soll_v_Yaw = inYawSollLage;
 
    //innerer Regler
@@ -355,7 +346,7 @@ void Regelung(void)
    ei_Yaw= Soll_v_Yaw - v_Yaw_tp1;	//Eingang in die innere Regelung
    pi_Yaw = ei_Yaw * 0.4;	//p-Anteil
    if(inSchub > 0.1 && inSchub <=1 && inYawIstLage < 0.2 && inYawIstLage > -0.2)
-   	ii_Yaw = 0.01 * ei_Yaw + ii_Yaw;	//I-Anteil nur nahe der Nulllage verändern 
+   	ii_Yaw = 0.01 * ei_Yaw + ii_Yaw;	//I-Anteil nur nahe der Nulllage verï¿½ndern 
 
    di_Yaw = (ei_Yaw - ei_Yaw_alt)*20; //d-Anteil  
    if (di_Yaw > 1.5)
@@ -392,7 +383,7 @@ void Regelung(void)
 		outMotor4 = 0.F;
    }						
    
-	/////////////////////////// Motorwerte saturieren und übergeben //////////////////////// 
+	/////////////////////////// Motorwerte saturieren und ï¿½bergeben //////////////////////// 
    if (outMotor1 > 6800.F) 	setMotor_1(6800.F);
    else if(outMotor1 < 0.F) setMotor_1(0.F);
 	 else setMotor_1(outMotor1);
@@ -448,13 +439,7 @@ static msg_t Regelungsthread(void *arg) {
 
 void setup_Regelung()
 {
-	//Regelungsthread anlegen
-	//Thread *tp = chThdCreateFromHeap(NULL, THD_WA_SIZE(128), NORMALPRIO+1, Regelungsthread, NULL);
-  //	if (tp == NULL)
-  //  	chSysHalt();    /* Memory exausted. */
-	tmObjectInit(&regelungdatalogsync_tmup);
-	chThdCreateStatic(RegelungSyncThreadWorkingArea, sizeof(RegelungSyncThreadWorkingArea), NORMALPRIO, RegelungSyncthread, NULL);
-	
-	chThdCreateStatic(RegelungPrintThreadWorkingArea, sizeof(RegelungPrintThreadWorkingArea), HIGHPRIO, RegelungPrintthread, NULL);
+	chThdCreateStatic(RegelungSyncThreadWorkingArea, sizeof(RegelungSyncThreadWorkingArea), HIGHPRIO, RegelungSyncthread, NULL);
+	chThdCreateStatic(RegelungPrintThreadWorkingArea, sizeof(RegelungPrintThreadWorkingArea), NORMALPRIO, RegelungPrintthread, NULL);
 	chThdCreateStatic(RegelungThreadWorkingArea, sizeof(RegelungThreadWorkingArea), ABSPRIO, Regelungsthread, NULL);
 }
