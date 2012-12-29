ec1e34b8ec7f5437c773806e8f0aae0485ad785b
EmbeddedFiedel <karsten-bade@web.de>
2012-12-29 02:21:58 +0100

    Implementierung Datenlog für Quad_Mini_Regelung
    - Mit Macbook erstellt, compilierbarkeit muss noch geprüft werden.
    - Faktoren für Umrechnung float -> int müssen für die einzelnen
    Variablen angepasst werden
    - Verschiebung der includes in die Header-Datei, damit regelung.cpp auf
    die Regelungsvariablen aus Regelglied usw zugreifen kann (Überprüfung
    noch offen)

--- Quad/Regelung.cpp
+++ Quad/Regelung.cpp
@@ -74,7 +74,7 @@ volatile unsigned short tmp111;
 #include "ff.h"
 
 	uint32_t regelung_timebuffer[50];
-	int regelung_databuffer[50][38];
+	int regelung_databuffer[50][20];
 	uint8_t readcounter = 0;
 	uint8_t writecounter = 0;
 	static FIL Fil_regelung;			/* File object */
@@ -112,7 +112,7 @@ static msg_t RegelungSyncthread(void *arg)
 				}
 				else
 				{
-					f_printf(&Fil_regelung, "Time_regelung;inSchub;inNickSollLage;inRollSollLage;inYawSollLage;inNickIstLage;inRollIstLage;inYawIstLage;inNickIstV;inRollIstV;inYawIstV;ea_Nick;ea_Roll;ea_Yaw;ia_Nick;ia_Roll;ia_Yaw;Soll_v_Nick;Soll_v_Roll;Soll_v_Yaw;ii_Nick;ii_Roll;ii_Yaw;pi_Nick;pi_Roll;pi_Yaw;di_Nick;di_Roll;di_Yaw;v_Nick_tp1;v_Roll_tp1;v_Yaw_tp1;aNick;aRoll;aYaw;outMotor1;outMotor2;outMotor3;outMotor4\r\n");	
+					f_printf(&Fil_regelung, "Time_regelung;In_Throttle;In_Soll_Nick;In_Soll_Roll;In_Soll_Gier;In_Ist_Nick;In_Ist_Roll;In_Ist_Gier;In_Ist_V_Nick;In_Ist_V_Roll;Out_M_Roll;Out_M_Nick;Out_M_Gier;Out_F_A;Out_F_B;Out_F_C;Out_F_D;Out_n_A;Out_n_B;Out_n_C;Out_n_D\r\n");	
 					rc_datalog = f_sync(&Fil_regelung);	
 					if(rc_datalog != FR_OK)
 					{
@@ -165,7 +165,7 @@ static msg_t RegelungPrintthread(void *arg)
 		{
 			systime_t time = chTimeNow();     // Tnow
 			chprintf((BaseChannel *) &SD2, "Printing:%d\r\n",time);
-			f_printf(&Fil_regelung, "%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d\r\n",
+			f_printf(&Fil_regelung, "%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d;%d\r\n",
 							regelung_timebuffer[readcounter],
 							regelung_databuffer[readcounter][0],
 							regelung_databuffer[readcounter][1],
@@ -186,25 +186,7 @@ static msg_t RegelungPrintthread(void *arg)
 							regelung_databuffer[readcounter][16],
 							regelung_databuffer[readcounter][17],
 							regelung_databuffer[readcounter][18],
-							regelung_databuffer[readcounter][19],
-							regelung_databuffer[readcounter][20],
-							regelung_databuffer[readcounter][21],
-							regelung_databuffer[readcounter][22],
-							regelung_databuffer[readcounter][23],
-							regelung_databuffer[readcounter][24],
-							regelung_databuffer[readcounter][25],
-							regelung_databuffer[readcounter][26],
-							regelung_databuffer[readcounter][27],
-							regelung_databuffer[readcounter][28],
-							regelung_databuffer[readcounter][29],
-							regelung_databuffer[readcounter][30],
-							regelung_databuffer[readcounter][31],
-							regelung_databuffer[readcounter][32],
-							regelung_databuffer[readcounter][33],
-							regelung_databuffer[readcounter][34],
-							regelung_databuffer[readcounter][35],
-							regelung_databuffer[readcounter][36],
-							regelung_databuffer[readcounter][37]);
+							regelung_databuffer[readcounter][19]);
 							readcounter++;
 							if(readcounter >=50)readcounter=0;
 		}
@@ -216,44 +198,26 @@ void datalog_regelung(void)
 {
 	uint32_t system_time = chTimeNow();
 	regelung_timebuffer[writecounter] = system_time;
-	regelung_databuffer[writecounter][0]=(int)(inSchub*100);
-	regelung_databuffer[writecounter][1]=(int)(inNickSollLage*100);
-	regelung_databuffer[writecounter][2]=(int)(inRollSollLage*100);
-	regelung_databuffer[writecounter][3]=(int)(inYawSollLage*100);
-	regelung_databuffer[writecounter][4]=(int)(inNickIstLage*100);
-	regelung_databuffer[writecounter][5]=(int)(inRollIstLage*100);
-	regelung_databuffer[writecounter][6]=(int)(inYawIstLage*100);
-	regelung_databuffer[writecounter][7]=(int)(inNickIstV*100);
-	regelung_databuffer[writecounter][8]=(int)(inRollIstV*100);
-	regelung_databuffer[writecounter][9]=(int)(inYawIstV*100);
-	regelung_databuffer[writecounter][10]=(int)(ea_Nick*100);
-	regelung_databuffer[writecounter][11]=(int)(ea_Roll*100);
-	regelung_databuffer[writecounter][12]=(int)(ea_Yaw*100);
-	regelung_databuffer[writecounter][13]=(int)(ia_Nick*100);
-	regelung_databuffer[writecounter][14]=(int)(ia_Roll*100);
-	regelung_databuffer[writecounter][15]=(int)(ia_Yaw*100);
-	regelung_databuffer[writecounter][16]=(int)(Soll_v_Nick*100);
-	regelung_databuffer[writecounter][17]=(int)(Soll_v_Roll*100);
-	regelung_databuffer[writecounter][18]=(int)(Soll_v_Yaw*100);
-	regelung_databuffer[writecounter][19]=(int)(ii_Nick*100);
-	regelung_databuffer[writecounter][20]=(int)(ii_Roll*100);
-	regelung_databuffer[writecounter][21]=(int)(ii_Yaw*100);
-	regelung_databuffer[writecounter][22]=(int)(pi_Nick*100);
-	regelung_databuffer[writecounter][23]=(int)(pi_Roll*100);
-	regelung_databuffer[writecounter][24]=(int)(pi_Yaw*100);
-	regelung_databuffer[writecounter][25]=(int)(di_Nick*100);
-	regelung_databuffer[writecounter][26]=(int)(di_Roll*100);
-	regelung_databuffer[writecounter][27]=(int)(di_Yaw*100);
-	regelung_databuffer[writecounter][28]=(int)(v_Nick_tp1*100);
-	regelung_databuffer[writecounter][29]=(int)(v_Roll_tp1*100);
-	regelung_databuffer[writecounter][30]=(int)(v_Yaw_tp1*100);
-	regelung_databuffer[writecounter][31]=(int)(aNick*100);
-	regelung_databuffer[writecounter][32]=(int)(aRoll*100);
-	regelung_databuffer[writecounter][33]=(int)(aYaw*100);
-	regelung_databuffer[writecounter][34]=(int)(outMotor1);
-	regelung_databuffer[writecounter][35]=(int)(outMotor2);
-	regelung_databuffer[writecounter][36]=(int)(outMotor3);
-	regelung_databuffer[writecounter][37]=(int)(outMotor4);
+	regelung_databuffer[writecounter][0]=(int)(Schubverteilung0_U.In_Throttle*100);
+	regelung_databuffer[writecounter][1]=(int)(Regelglied_U.In_Soll_Nick*100);
+	regelung_databuffer[writecounter][2]=(int)(Regelglied_U.In_Soll_Roll*100);
+	regelung_databuffer[writecounter][3]=(int)(Regelglied_U.In_Soll_Gier*100);
+	regelung_databuffer[writecounter][4]=(int)(Regelglied_U.In_Ist_Nick*100);
+	regelung_databuffer[writecounter][5]=(int)(Regelglied_U.In_Ist_Roll*100);
+	regelung_databuffer[writecounter][6]=(int)(Regelglied_U.In_Ist_Gier*100);
+	regelung_databuffer[writecounter][7]=(int)(Regelglied_U.In_Ist_V_Nick*100);
+	regelung_databuffer[writecounter][8]=(int)(Regelglied_U.In_Ist_V_Roll*100);
+	regelung_databuffer[writecounter][9]=(int)(Regelglied_Y.Out_M_Roll*100);
+	regelung_databuffer[writecounter][10]=(int)(Regelglied_Y.Out_M_Nick*100);
+	regelung_databuffer[writecounter][11]=(int)(Regelglied_Y.Out_M_Gier*100);
+	regelung_databuffer[writecounter][12]=(int)(Schubverteilung0_Y.Out_F_A*100);
+	regelung_databuffer[writecounter][13]=(int)(Schubverteilung0_Y.Out_F_B*100);
+	regelung_databuffer[writecounter][14]=(int)(Schubverteilung0_Y.Out_F_C*100);
+	regelung_databuffer[writecounter][15]=(int)(Schubverteilung0_Y.Out_F_D*100);
+	regelung_databuffer[writecounter][16]=(int)(Inverse_Propeller_Y.Out_n_A*100);
+	regelung_databuffer[writecounter][17]=(int)(Inverse_Propeller_Y.Out_n_B*100);
+	regelung_databuffer[writecounter][18]=(int)(Inverse_Propeller_Y.Out_n_C*100);
+	regelung_databuffer[writecounter][19]=(int)(Inverse_Propeller_Y.Out_n_D*100);
 	
 	writecounter++;
 	
