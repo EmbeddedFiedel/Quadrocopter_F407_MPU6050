306a4551c0ef5d37169052ef58d67418df16a02f
Dennis <langedennis@web.de>
2013-01-02 16:36:35 +0100

    Fehler beseitigt + getestet Achtung: serial.h Buffer_Size auf 127 erhöht. Muss das?!

--- Project/ARMCM4-STM32F407-DISCOVERY/main.c
+++ Project/ARMCM4-STM32F407-DISCOVERY/main.c
@@ -39,49 +39,52 @@
 #include "mavlink.h"
 #include "common.h"
 
+
+uint16_t len;
+
 void send_heartbeat(void)
 {
 	mavlink_system_t mavlink_system;
  
-mavlink_system.sysid = 1;                   ///< ID 1 for this airplane
-mavlink_system.compid = MAV_COMP_ID_ALL;     ///< The component sending the message is the IMU, it could be also a Linux process
- 
-// Define the system type, in this case an airplane
-uint8_t system_type = MAV_TYPE_QUADROTOR;
-uint8_t autopilot_type = MAV_AUTOPILOT_GENERIC;
- 
-uint8_t system_mode = MAV_MODE_STABILIZE_ARMED; ///< Booting up
-uint32_t custom_mode = 0;                 ///< Custom mode, can be defined by user/adopter
-uint8_t system_state = MAV_STATE_ACTIVE; ///< System ready for flight
- 
-// Initialize the required buffers
-mavlink_message_t msg;
-uint8_t buf[MAVLINK_MAX_PACKET_LEN];
- 
-// Pack the message
-mavlink_msg_heartbeat_pack(mavlink_system.sysid, mavlink_system.compid, &msg, system_type, autopilot_type, system_mode, custom_mode, system_state);
- 
-// Copy the message to the send buffer
-uint16_t len = mavlink_msg_to_send_buffer(buf, &msg);
- 
-// Send the message with the standard UART send function
-// uart0_send might be named differently depending on
-// the individual microcontroller / library in use.
-sdWrite(&SD2, buf, len);
+	mavlink_system.sysid = 1;                   ///< ID 1 for this airplane
+	mavlink_system.compid = MAV_COMP_ID_ALL;     ///< The component sending the message is the IMU, it could be also a Linux process
+	 
+	// Define the system type, in this case an airplane
+	uint8_t system_type = MAV_TYPE_QUADROTOR;
+	uint8_t autopilot_type = MAV_AUTOPILOT_GENERIC;
+	 
+	uint8_t system_mode = MAV_MODE_STABILIZE_ARMED; ///< Booting up
+	uint32_t custom_mode = 0;                 ///< Custom mode, can be defined by user/adopter
+	uint8_t system_state = MAV_STATE_ACTIVE; ///< System ready for flight
+	 
+	// Initialize the required buffers
+	mavlink_message_t msg;
+	uint8_t buf[MAVLINK_MAX_PACKET_LEN];
+	 
+	// Pack the message
+	mavlink_msg_heartbeat_pack(mavlink_system.sysid, mavlink_system.compid, &msg, system_type, autopilot_type, system_mode, custom_mode, system_state);
+	 
+	// Copy the message to the send buffer
+	len = mavlink_msg_to_send_buffer(buf, &msg);
+	// Send the message with the standard UART send function
+	// uart0_send might be named differently depending on
+	// the individual microcontroller / library in use.
+	sdAsynchronousWrite(&SD2, buf, len);
 }
 
 /*
  * Working area for MavlinkHeartbeatThread
  */
-static WORKING_AREA(MavlinkHeartbeatThreadWorkingArea, 128);
+static WORKING_AREA(MavlinkHeartbeatThreadWorkingArea, 4096);
 /*
  * MavlinkHeartbeatThread
  */
+
 static msg_t MavlinkHeartbeatThread(void *arg) 
 {
   while (TRUE) 
-	{
-		send_heartbeat();
+  {
+		send_heartbeat();	 
 		chThdSleepMilliseconds(1000);
   }
 }
--- os/hal/include/serial.h
+++ os/hal/include/serial.h
@@ -78,7 +78,7 @@
  *          buffers.
  */
 #if !defined(SERIAL_BUFFERS_SIZE) || defined(__DOXYGEN__)
-#define SERIAL_BUFFERS_SIZE         16
+#define SERIAL_BUFFERS_SIZE         127
 #endif
 /** @} */
 
